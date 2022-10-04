6a845f8f78f5ef00ecbd19350f399f253e59be98
Владимир Колесников <v.kolesnikov@ideco.ru>
2022-10-04 08:08:35 +0500

    dist path

--- start.js
+++ start.js
@@ -5,11 +5,11 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(express.static(__dirname));
-app.use(express.static(path.resolve(__dirname, 'build')));
+app.use(express.static(path.resolve(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
   if(!req.path.includes('/api/')) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
   }
 });
 
